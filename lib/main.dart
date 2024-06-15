4f2e24ef8cdadc9aeec4d8b56f8dc6563ae34c7b
CristinaQM <cequinte@espol.edu.ec>
2024-06-15 17:29:56 -0500

    Conexión a Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:temperature_map/routes/pages.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
